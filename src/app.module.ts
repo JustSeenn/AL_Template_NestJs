a4d007c09cba3864dad9016d36ca800efc89d729
I576958 <vincent.boillot.devalliere@sap.com>
2023-05-19 13:58:06 +0200

    Feature - Postgres DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getEnvPath } from './common/helper/env.helper';
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ConfigModule.forRoot({ envFilePath, isGlobal: true }), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
