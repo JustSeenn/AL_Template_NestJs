7ac42aec1bd496899a9866cf6085b2960b3f2184
I576958 <vincent.boillot.devalliere@sap.com>
2023-05-19 14:51:18 +0200

    Fix - ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 2021,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -70,15 +65,5 @@ module.exports = {
     // Comments
     'spaced-comment': ['error', 'always'], // Require space after the comment start characters
 
-    // Variable naming
-    '@typescript-eslint/naming-convention':
-      - 'error'
-      - {
-        'selector': 'variable',
-        'format': ['camelCase', 'UPPER_CASE']
-      } // Use camelCase or UPPER_CASE for variable names
-
-    // Other rules specific to your code style
-    // ...
-    },
+  }
 };
